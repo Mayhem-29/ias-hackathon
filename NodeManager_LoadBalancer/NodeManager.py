f4d639b104b0e0f1af8613140625d5f521ca9a15
IraTyagi31 <ira.tyagi@students.iiit.ac.in>
2022-04-03 16:25:26 +0530

    Node Manager and Load Balancer

---
+++ NodeManager_LoadBalancer/NodeManager.py
@@ -0,0 +1,102 @@
+from flask import Flask, request
+import requests
+
+session = requests.Session()
+app = Flask(__name__)
+# myclient=pymongo.MongoClient("mongodb+srv://hackathon:hackathon@hackathon.wgs03.mongodb.net/Hackathon?retryWrites=true&w=majority")
+# mydb=myclient["Hackathon"]
+# mycollection=mydb["Request_db"]
+
+SENSOR_PORT = 9100
+MODEL_PORT = 9200
+LOAD_PORT = 9300
+APP_PORT = 9400
+DEPLOYER_PORT = 9500
+NODE_PORT = 9500
+SCH_PORT = 9600
+
+
+endpoint = {
+    "sensor_manager": {
+        "base_url": "http://localhost:"+str(SENSOR_PORT), 
+        "uri": {
+            "sensorinfo": "/sensorinfo",
+            "getsensordata": "/getsensordata"
+        }
+    },
+    "app_manager": {
+        "base_url": "http://localhost:" + str(APP_PORT),
+        "uri": {
+            "get_all_models_sensos": "/get_models_sensors",
+            "get_all_apps": "/get_all_applications",
+            "get_sensor_by_app_id": "/get_sensor_by_app_id",
+            "deploy_app": "/deploy"
+        }
+    },
+    "load_balancer": {
+        "base_url": "http://localhost:" + str(LOAD_PORT),
+        "uri": {
+            "get_node_id": "/get_node_id"
+        }
+    },
+    "deployer": {
+        "base_url": "http://localhost:" + str(DEPLOYER_PORT),
+        "uri": {
+            "send_to_deployer": "/send_to_deployer",
+        }
+    },
+}
+
+
+@app.route("/get_schedule_app", methods=["POST"])
+def get_schedule_app():
+    req = request.get_json()
+    """
+    if end_status=0:
+        req={
+            app_inst_id: str,
+            stand_alone: bool,
+            end_status: 0
+        }
+    else:
+        req={
+            app_inst_id: str,
+            end_status: 1
+        }
+
+    1)if end_status==0,
+        call load balancer and give stand_alone status(no use yet)
+        get node id and app_inst_id to give to deployer
+    2)if end_status==1,
+        give empty node id and app_inst_id to deployer
+    """
+
+    deploy = {
+        "app_inst_id" : req["app_inst_id"],
+        "node_id":""
+        }
+
+    if req["end_status"]==0:
+        load_reply = session.post(endpoint['load_balancer']['base_url'] + endpoint['load_balancer']['uri']['get_node_id'],json={'stand_alone':req['stand_alone']}).json()
+        """
+        load_reply={
+            'node_id': str
+            'message': str
+        }
+        """
+        node_id=load_reply['node_id']
+        deploy["node_id"]=node_id
+        print(load_reply["message"])
+
+    session.post(endpoint['deployer']['base_url'] + endpoint['deployer']['uri']['send_to_deployer'], json=deploy).json()
+    """
+    status={
+        "status"
+        "message"
+    }
+    """
+    return status
+
+
+if __name__=="__main__":
+    app.run(port=NODE_PORT, debug=True)
--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -7,36 +7,62 @@ import math
 import heapq
 
 app = Flask(__name__)
+
+"""
+Database for fault tolerance
+"""
 myclient=pymongo.MongoClient("mongodb+srv://hackathon:hackathon@hackathon.wgs03.mongodb.net/Hackathon?retryWrites=true&w=majority")
 mydb=myclient["Hackathon"]
-mycollection=mydb["Request_db"]
+mycollection=mydb["Scheduler_db"]
 sess = requests.Session()
 
 SENSOR_PORT = 9100
 MODEL_PORT = 9200
-PLATFORM_PORT = 9300
+LOAD_PORT = 9300
 APP_PORT = 9400
 DEPLOYER_PORT = 9500
 NODE_PORT = 9500
 SCH_PORT = 9600
 
 
-DeployerURL="http://localhost:8080"
 
-uri={
-    "userInput":"/get_schedule_app"
+endpoint = {
+    "sensor_manager": {
+        "base_url": "http://localhost:"+str(SENSOR_PORT), 
+        "uri": {
+            "sensorinfo": "/sensorinfo",
+            "getsensordata": "/getsensordata"
+        }
+    },
+    "node_manager": {
+        "base_url": "http://localhost:" + str(NODE_PORT),
+        "uri": {
+            "get_schedule_app": "/get_schedule_app"
+        }
+    },
+    "load_balancer": {
+        "base_url": "http://localhost:" + str(LOAD_PORT),
+        "uri": {
+            "get_node_id": "/get_node_id"
+        }
+    },
+    "deployer": {
+        "base_url": "http://localhost:" + str(DEPLOYER_PORT),
+        "uri": {
+            "send_to_deployer": "/send_to_deployer",
+        }
+    },
+    "app_manager": {
+        "base_url": "http://localhost:" + str(APP_PORT),
+        "uri": {
+            "get_all_models_sensos": "/get_models_sensors",
+            "get_all_apps": "/get_all_applications",
+            "get_sensor_by_app_id": "/get_sensor_by_app_id",
+            "deploy_app": "/deploy"
+        }
+    },
 }
 
-scheduler_endpoints = {
-    "base_url": "http://localhost:" + str(DEPLOYER_PORT),
-    "uri": {
-        "userInput": "/get_schedule_app"
-    }
-}
-
-"""
-Database for requests
-"""
 
 Scheduler_queue=[]
 Termination_queue=[]
@@ -44,21 +70,26 @@ Termination_queue=[]
 @app.route('/sendInfo',methods=['POST'])
 def getUserInput():
     response=request.get_json()
-    result={"status":"false"}
     """
-    1)take this response object and pass it to request db as it is
-    2)get the request id 
-    3)take start time, end time, request id
+    response={
+        app_inst_id: str
+        start_time: str
+        end_time: float
+        stand_alone: bool
+    }
+    1)add response to Scheduler_db
+    2)take start time, end time, request id
       andadd to first priority queue 
     """
     #1
-    doc=mycollection.insert_one(response)
+    mycollection.insert_one(response)
    
     #2
-    request_id=doc.inserted_id
-    #3
-    heapq.heappush(Scheduler_queue,(response["start_time"],response["end_time"],request_id))
-    result["status"]="true"
+    heapq.heappush(Scheduler_queue,(response["start_time"],response["end_time"],response["app_ins_id"],response["stand_alone"]))
+    result={
+        "status":"true",
+        "message":"Application Scheduled..."
+    }
     
     return json.dumps(result)
 
@@ -66,21 +97,23 @@ def getUserInput():
 
 def deployerApp(appInfo):
     deployer_obj={}
-    deployer_obj["request_id"]=str(appInfo[2])
+    deployer_obj["app_inst_id"]=appInfo[2]
+    deployer_obj["stand_alone"]=appInfo[3]
     deployer_obj["end_status"]=0
 
-    # print("72...sending to deployer to deploy")
-    response=sess.post(url= scheduler_endpoints["base_url"] +scheduler_endpoints["uri"]["userInput"],json = deployer_obj).json()
+    # print("72...sending to node manager to deploy")
+    response=sess.post(endpoint['node_manager']['base_url'] + endpoint['node_manager']['uri']['get_schedule_app'],json=deployer_obj).json()
 
     """
     1)response status = true
         1.1)calculate the end time
-        1.2)take end time, req id
+        1.2)take end time, req id, stand_alone,
             and add to the second priority queue
-    2)remove the corresponding req id entry from db
+    2)delete entry from Scheduler_db since the app is now scheduled and deployed
     """
 
     #1
+    print(response["message"]) #"App is deployed or not"
     if response["status"]=="true":
         #1.1
         duration=float(appInfo[1])
@@ -94,11 +127,10 @@ def deployerApp(appInfo):
         end_time = end_time.rsplit(':',2)[0]
         #this end_time is  String
         #1.2
-        heapq.heappush(Termination_queue,(end_time,deployer_obj["request_id"]))
+        heapq.heappush(Termination_queue,(end_time,appInfo[2]))
     
     #2
-    # print("99")
-    mycollection.delete_one({"_id": appInfo[2]})
+    mycollection.delete_one({"app_inst_id": appInfo[2]})
    
 
 
@@ -109,6 +141,7 @@ def scheduling_function():
             while str(datetime.now()).rsplit(':',1)[0]<Scheduler_queue[0][0]:
                 pass
             appInfo = heapq.heappop(Scheduler_queue)
+            #appInfo is a tuple that has 4 fields : start_time, end_time, app_inst_id, stand_alone
             deployerApp(appInfo)
 
 def termination_function():
@@ -119,12 +152,12 @@ def termination_function():
             while str(datetime.now()).rsplit(':',1)[0]<Termination_queue[0][0]:
                 pass
             # print("121 termination function ",str(datetime.now()).rsplit(':',1)[0])
-            kill_id = heapq.heappop(Termination_queue)[1]
+            appInstId = heapq.heappop(Termination_queue)[1]
             deployer_obj={}
-            deployer_obj["request_id"]=kill_id
+            deployer_obj["app_inst_id"]=appInstId
             deployer_obj["end_status"]=1
-            response=sess.post(url= scheduler_endpoints["base_url"] +scheduler_endpoints["uri"]["userInput"],json = deployer_obj).json()
-            # print(response)
+            response=sess.post(endpoint['node_manager']['base_url'] + endpoint['node_manager']['uri']['get_schedule_app'],json=deployer_obj).json()
+            print(response["message"])#application killed or not
 
 if __name__=="__main__":
     """
