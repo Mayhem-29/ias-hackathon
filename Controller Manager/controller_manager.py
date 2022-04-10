36754e315d4ce7f2a85fe36b424d8e1c530e41f4
Ashish Rai <ashishrai96@yahoo.com>
2022-04-10 06:52:33 +0530

    added controller

---
+++ Controller Manager/controller_manager.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+import json, os
+
+from requests import request
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config['SECRET_KEY'] = "dub_nation"
+
+DB_SERVER = "mongodb+srv://hackathon:hackathon@hackathon.wgs03.mongodb.net/Hackathon?retryWrites=true&w=majority"
+client = MongoClient(DB_SERVER)
+HACKATHON_DB = client['Hackathon']
+
+ctrl_type_db = HACKATHON_DB['ControllerTypeDB']
+ctrl_instance_db = HACKATHON_DB['ControllerInstanceDB']
+
+def read_json(file_name):
+    with open(file_name, "r") as f:
+        return json.load(f)
+
+constants = read_json("constants.json")
+
+
+def init_controller():
+    ctrl_instance_list = ctrl_instance_db.find()
+    if ctrl_instance_list and len(ctrl_instance_list) > 0:
+        for ctrl_instance in ctrl_instance_list:
+            os.system(f"""sshpass -p Yash@1998 ssh yash@localhost 
+                'gnome-terminal python3 controller_instance.py {ctrl_instance["_id"]}'
+            """.format(ctrl_instance_id=ctrl_instance["_id"]))
+
+
+@app.route("/")
+def hello_controller():
+    return render_template("controller_home.html",
+        home = constants["BASE_URL"] + constants["PORT"]["APP_PORT"] + constants["ENDPOINTS"]["APP_MANAGER"]["home"],
+        add_ctrl_type_api = constants["BASE_URL"] + constants["PORT"]["CTRL_PORT"] + constants["ENDPOINTS"]["CONTROLLER_MANAGER"]["add_controller_type"],
+        add_ctrl_instance_api = constants["BASE_URL"] + constants["PORT"]["CTRL_PORT"] + constants["ENDPOINTS"]["CONTROLLER_MANAGER"]["add_controller_instance"],
+        sensor_url = constants["BASE_URL"] + constants["PORT"]["SENSOR_PORT"] + constants["ENDPOINTS"]["SENSOR_MANAGER"]["admin"],
+    )
+
+
+@app.route("add_controller_type", methods=["POST"])
+def add_controller_type():
+    ctrl_name = request.form.get("ctrl_name")
+    ctrl_op = request.form.get("ctrl_op")
+
+    ctrl_doc = ctrl_type_db.find_one({"ctrl_name": ctrl_name})
+    if ctrl_doc:
+        return jsonify({'message': 'Controller type already exists', 'status_code': 500}), 500
+    
+    ctrl_type_db.insert_one({"ctrl_name": ctrl_name, "ctrl_op": ctrl_op})
+    return jsonify({'message': 'Controller type added successfully', 'status_code': 200}), 200
+
+
+@app.route("get_controller_list", methods=["POST"])
+def get_controller_list():
+    ctrl_list = ctrl_type_db.find()
+    ctrl_list_json = []
+    for ctrl in ctrl_list:
+        ctrl_list_json.append(ctrl)
+    return jsonify(ctrl_list_json), 200
+
+
+@app.route("add_controller_instance", methods=["POST"])
+def add_controller_instance():
+    ctrl_type = request.form.get("ctrl_type")
+    location = request.form.get("location")
+    ctrl_ip = request.form.get("ctrl_ip")
+    ctrl_port = request.form.get("ctrl_port")
+
+    try:
+        ctrl_instance_db.insert_one({"ctrl_type": ctrl_type, "location": location, "ctrl_ip": ctrl_ip, "ctrl_port": ctrl_port})
+        return jsonify({'message': 'Controller instance added successfully', 'status_code': 200}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Unable to add controller instance', 'status_code': 500}), 500
+
+
+@app.route("get_controller_instance_list", methods=["POST"])
+def get_controller_instance_list():
+    try:
+        ctrl_list = ctrl_instance_db.find()
+        ctrl_list_json = []
+        for ctrl in ctrl_list:
+            ctrl_list_json.append(ctrl)
+        return jsonify(ctrl_list_json), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Unable to get controller instance list', 'status_code': 500}), 500
+
+
+if __name__ == "__main__":
+    init_controller()
+    app.run(port=constants["PORT"]["CTRL_PORT"], debug=True)
